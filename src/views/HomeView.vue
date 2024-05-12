a8cb2bdfb1cdd8227df62fa7ddfc139530bca805
Alberto Flores <hostlindev@gmail.com>
2024-05-12 18:47:47 -0500

    [added] Agregado componente de categorias en la pagina de homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import ProductList from "@/components/ProductList.vue";
+import CategoryList from "@/components/CategoryList.vue";
+
 </script>
 
 <template>
   <main>
-    <ProductList />
+    <v-row>
+      <v-col cols="2">
+        <CategoryList />
+      </v-col>
+
+      <v-col cols="10">
+        <ProductList />
+      </v-col>
+    </v-row>
+
   </main>
 </template>
