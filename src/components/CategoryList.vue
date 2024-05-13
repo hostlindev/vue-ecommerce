85c1de2ca85005c441420cf7fb8bc85b334312eb
Alberto Flores <hostlindev@gmail.com>
2024-05-12 19:06:55 -0500

    [added] Agregado lista personalizada de categorias

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -1,15 +1,29 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import type { Category } from '@/model/types';
+
+const categories = ref(<Array<Category>>[
+    {
+        id: 1,
+        name: 'Technology'
+    },
+    {
+        id: 2,
+        name: 'Toys'
+    }
+])
 
 </script>
 
 <template>
     <v-sheet rounded="lg">
         <v-list rounded="lg">
-            <v-list-item v-for="n in 5" :key="n" :title="`List Item ${n}`" link></v-list-item>
+            <v-list-item v-for="category in categories" :key="category.id" :title="`${category.name}`"
+                link></v-list-item>
 
             <v-divider class="my-2"></v-divider>
 
-            <v-list-item color="grey-lighten-4" title="Refresh" link></v-list-item>
+            <v-list-item color="grey-lighten-4" title="Order by price" link></v-list-item>
         </v-list>
     </v-sheet>
 </template>
